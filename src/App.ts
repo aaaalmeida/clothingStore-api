86dc0d89bb02356af1ec1f9b625385bad9e5a033
aaaalmeida <arthurfonsecaalmeida@gmail.com>
2024-08-03 05:36:52 -0300

    add .net support

--- src/App.ts
+++ src/App.ts
@@ -1,5 +1,16 @@
-interface Pessoa {
-  name: string
+require('dotenv/config')
+const url = process.env.MONGODB_URL;
+
+import { MongoClient } from 'mongodb';
+const client = new MongoClient(url!);
+async function run() {
+  try {
+    const query = { "title": "In the Land of the Head Hunters" }
+    const projection = { title: true, genres: true, year: true }
+    console.log(await client.db('sample_mflix').collection('movies').findOne(query, { projection }))
+  } finally {
+    await client.close()
+  }
 }
-const arthur: Pessoa = {name: 'abc'}
-console.log(arthur.name)
+
+run().catch(console.dir)
