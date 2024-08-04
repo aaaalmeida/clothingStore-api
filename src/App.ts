1f12e86a694ae06fe388e830489ef6f04937beb9
aaaalmeida <arthurfonsecaalmeida@gmail.com>
2024-08-04 05:25:04 -0300

    fixed route not receiving actual query response

--- src/App.ts
+++ src/App.ts
@@ -1,25 +1,40 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import dotenv from 'dotenv'
+dotenv.config();
 
-app.get('/', (req, res) => {
-  res.send(`Ola mundo ${port}`)
+const port = process.env.SERVER_PORT;
+const url = process.env.MONGODB_URL;
+
+if (!url) {
+  throw new Error("MONGODB_URL not defined");
+}
+
+import express from 'express';
+import { MongoClient } from 'mongodb';
+const mongoClient = new MongoClient(url!);
+
+const run = async () => {
+  try {
+    await mongoClient.connect();
+    const query = { "title": "In the Land of the Head Hunters" }
+    const projection = { title: true, genres: true, year: true }
+    const response = await mongoClient.db('sample_mflix').collection('movies').findOne(query, { projection })
+    
+    return response
+  } catch (err) {
+    console.dir(err)
+  }
+  finally {
+    await mongoClient.close()
+  }
+}
+
+const server = express()
+
+server.get('/', async (req, res) => {
+  const movie = await run()
+  res.send(`Ola mundo ${JSON.stringify(movie)}`)
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Servidor na porta ${port}`)
-}).finally(app.close())
-// import { MongoClient } from 'mongodb';
-// const client = new MongoClient(url!);
-
-// async function run() {
-//   try {
-//     const query = { "title": "In the Land of the Head Hunters" }
-//     const projection = { title: true, genres: true, year: true }
-//     console.log(await client.db('sample_mflix').collection('movies').findOne(query, { projection }))
-//   } finally {
-//     await client.close()
-//   }
-// }
-
-// run().catch(console.dir)
+})
